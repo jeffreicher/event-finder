2fec081226008dcb0955bb31aff83eda1bce0143
Keaton Krieger <bajabuggy313@gmail.com>
2018-03-07 12:41:10 -0800

    Got list of events and list of artist's. Working on getting first image for each artist :D

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ $(document).ready(initializeApp);
  * hiphop_array - global array to hold hiphop info objects
  * @type {Array}
  */
-var hiphop_array = []
+var hiphop_array = [];
+var keyword ='electronic';
+var artistInfo = [];
+var artistImg = [];
 
  /***************************************************************************************************
 * initializeApp 
@@ -16,7 +19,8 @@ var hiphop_array = []
 * initializes the application, including adding click handlers and pulling in any data from the server, in later versions
 */
 function initializeApp(){
-    addClickHandlersToElements();      
+    addClickHandlersToElements();
+     getDataFromTicketMaster(keyword);
 }
 
 /***************************************************************************************************
@@ -27,4 +31,43 @@ function initializeApp(){
 */
 function addClickHandlersToElements(){     
    
+}
+
+function getDataFromTicketMaster (keyword) {
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events?apikey=tBBObsl2YtXpvAceOW6DOKwRtZpd8bxd&keyword="+keyword+"&countryCode=US&stateCode=Ca",
+        dataType: "text",
+        success: function (json) {
+            json = JSON.parse(json);
+            for(var i =0; i < json._embedded.events.length; i++){
+                var fesivalObjects = json._embedded.events[i];
+                hiphop_array.push(fesivalObjects);
+            }
+            getArtistFromEvents();
+            // Parse the response.
+            // Do other things.
+        },
+        error: function (xhr, status, err) {
+            // This time, we do not end up here!
+        }
+    });
+}
+
+function getArtistFromEvents() {
+    for(var i=0; i<hiphop_array.length; i++){
+        var attraction = hiphop_array[i]._embedded.attractions;
+        artistInfo.push(attraction);
+        }
+        getArtistImages();
+}
+
+function getArtistImages (){
+    for(var x=0; x<artistInfo.length; x++){
+        for()
+            var img = [];
+                artistInfo[x][0].images[0].url;
+            artistImg.push(img);
+        // console.log(x)
+        }
 }
