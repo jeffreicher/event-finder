f2ed101afbc8ed8752ef05edbace31fda89feedc
Hanran <hanrankim@gmail.com>
2018-03-07 10:21:23 -0800

    created html, js, css files to start

---
+++ main.js
@@ -0,0 +1,30 @@
+/**
+ * Listen for the document to load and initialize the application
+ */
+$(document).ready(initializeApp);
+
+/***********************
+ * hiphop_array - global array to hold hiphop info objects
+ * @type {Array}
+ */
+var hiphop_array = []
+
+ /***************************************************************************************************
+* initializeApp 
+* @params {undefined} none
+* @returns: {undefined} none
+* initializes the application, including adding click handlers and pulling in any data from the server, in later versions
+*/
+function initializeApp(){
+    addClickHandlersToElements();      
+}
+
+/***************************************************************************************************
+* addClickHandlerstoElements
+* @params {undefined} 
+* @returns  {undefined}
+*     
+*/
+function addClickHandlersToElements(){     
+   
+}
