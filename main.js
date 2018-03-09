bd6a6ee1dbba99519a083153fb3a05933f5df92f
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-09 00:30:23 -0800

    Added comments and youtube settings

--- main.js
+++ main.js
@@ -29,16 +29,16 @@ var ticketObject = {
  * initializes the application, including adding click handlers and pulling in any data from the server, in later versions
  */
 function initializeApp() {
-    addClickHandlersToElements();
+    addClickHandlersAndStylesToElements();
     createPlayer();
 }
 
 /***************************************************************************************************
- * addClickHandlerstoElements
+ * addClickHandlersAndStylesToElements
  * @params {undefined}
  * @returns  {undefined}
  */
-function addClickHandlersToElements() {
+function addClickHandlersAndStylesToElements() {
     $('.search-events').on('click', getDataFromTicketMaster);
     $('.backButton').on('click', backButtonActions);
     $('.secondScreen').addClass('hidden');
@@ -307,30 +307,6 @@ function getArtistFromEvents() {
         getArtistImages();
 }
 
-
-function loadVideo(name) {
-    $.ajax({
-        dataType: 'json',
-        data: {
-            api_key: 'IkLZGbSrRC',
-            q: name + ' live set',
-            maxResults: 5,
-            type: 'video'
-        },
-        method: 'POST',
-        url: 'https://s-apis.learningfuze.com/hackathon/youtube/search.php',
-        success: function(response){
-            if(response.success){
-                videoIdArray=[];
-                console.log('success', response);
-                for ( var i = 0; i < response.video.length; i++) {
-                    videoIdArray.push(response.video[i].id);
-                }
-                changePlayer(videoIdArray[0]);
-            }
-        }
-    });
-}
 //keaton
 function getDataFromTicketMaster() {
     var keyword = $('#genre')[0];
@@ -375,6 +351,7 @@ function getDataFromTicketMaster() {
         }
     });
 }
+
 function updateEventsLists(events_array) {
     var tbody = $('<tbody>').addClass('table-content');   
     var table = $('<table>').addClass('events-lists');  
@@ -412,13 +389,17 @@ function updateEventsLists(events_array) {
       $('.left-col').prepend(table);
 }
 
+//how does this work with the youtubeframapiready?
 function createPlayer() {
+    debugger;
     var tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
+//loads new YouTube player with properties and stores in video player global 
+//Error parsing header X-XSS-Protection is a bug found in chrome
 function onYouTubeIframeAPIReady() {
     videoPlayer = new YT.Player('player', {
         height: '345',
@@ -426,11 +407,38 @@ function onYouTubeIframeAPIReady() {
         videoId: 'L6c_mYQ9LaM',
         playerVars: {
             'autoplay': 1,
-            'start': 1
-
+            'controls': 0,
+            'loop': 1,
+            'modestbranding': 1,
         }
     });
 }
+
+// loads video from learningfuze server using an api key to that if is successful, stores videos in a video array and calls changeplayer with video array at 0
+function loadVideo(name) {
+    $.ajax({
+        dataType: 'json',
+        data: {
+            api_key: 'IkLZGbSrRC',
+            q: name + ' live set',
+            maxResults: 5,
+            type: 'video'
+        },
+        method: 'POST',
+        url: 'https://s-apis.learningfuze.com/hackathon/youtube/search.php',
+        success: function(response){
+            if(response.success){
+                videoIdArray=[];
+                for ( var video_i = 0; video_i < response.video.length; video_i++) {
+                    videoIdArray.push(response.video[video_i].id);
+                }
+                changePlayer(videoIdArray[0]);
+            }
+        }
+    });
+}
+
+//changes the video played by changing the video Id
 function changePlayer(newID){
     videoPlayer.a.src = 'https://www.youtube.com/embed/'+newID+'?start=1';
 }
