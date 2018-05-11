a3ef51a3efecb6a00bcb37e95e30acc60b8a2bab
Queen Bae <sashakyunghwa@gmail.com>
2018-05-11 13:04:55 -0700

    dynamic flickr

--- main.js
+++ main.js
@@ -68,216 +68,57 @@ class MusicConcert {
         flickr_pic.attr("src", img_src);
     }
 
+    var refList = {
+        "Mattress Firm Amphitheatre": "37522480132",
+        "Mattress Firm Amphitheatre (Formerly Sleep Train Amphitheatre)": "37522480132",
+        "The Observatory - Santa Ana": "22927627330",
+        "Teragram Ballroom": "30043253042",
+        "House of Blues Anaheim": "8040060204",
+        "Mezzanine": "4484999982",
+        "Fox Theater - Oakland": "31142080295",
+        "Greek Theatre-U.C. Berkeley": "35489133763",
+        "Greek Theatre": "35489133763",
+        "City National Civic": "22592033217",
+        "Bill Graham Civic Auditorium": "30479561483",
+        "Hollywood Palladium": "27020496362",
+        "Hollywood Pantages Theatre": "31084523630",
+        "FivePoint Amphitheatre": "36211111572",
+        "Shoreline Amphitheatre": "39379605704",
+        "Toyota Amphitheatre": "27191296995",
+        "Copper Blues Oxnard Resturant": "",
+        "Dodger Stadium": "",
+        "Save Mart Center": "",
+        "Honda Center": "",
+        "The Forum": "",
+        "The Pacific Amphitheatre": "",
+        "Pechanga Resort and Casino": "",
+        "William Saroyan Theatre Fresno Convention & Entertainment Center": ""
+    };
+
+
     flickrLoop(venueLocation) {
-        var venueImages =
-            {
-                "photos": {
-                    "photo": [
-                        // Hollywood Palladium
-                        {
-                            "id": "27020496362",
-                            "owner": "94467141@N00",
-                            "secret": "32cb198536",
-                            "server": "7070",
-                            "farm": 8,
-                            "title": "Stevie Nicks and Dave Grohl, ladies...",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                            ,
-                        },
-                        // Bill Graham Civic Auditorium
-                        {
-                            "id": "30479561483",
-                            "owner": "141233868@N02",
-                            "secret": "31493d3001",
-                            "server": "5715",
-                            "farm": 6,
-                            "title": "Bill Graham Civic Auditorium",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // City National Civic
-                        {
-                            "id": "22592033217",
-                            "owner": "58112562@N00",
-                            "secret": "9700cbbcc0",
-                            "server": "5689",
-                            "farm": 6,
-                            "title": "City National Civic",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Greek Theatre
-                        {
-                            "id": "35489133763",
-                            "owner": "7948216@N05",
-                            "secret": "9c28d0e767",
-                            "server": "4303",
-                            "farm": 5,
-                            "title": "07-22-2017 Berkeley, CA - Greek Theatre",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Fox Theater
-                        {
-                            "id": "31142080295",
-                            "owner": "34457978@N00",
-                            "secret": "5b455ec238",
-                            "server": "5764",
-                            "farm": 6,
-                            "title": "Fox Theater (Oakland)",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Mezzanine
-                        {
-                            "id": "4484999982",
-                            "owner": "49005895@N08",
-                            "secret": "98681bd752",
-                            "server": "4008",
-                            "farm": 5,
-                            "title": "Mezzanine Private Event Venue San Francisco",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // House of Blues Anaheim
-                        {
-                            "id": "8040060204",
-                            "owner": "72756218@N04",
-                            "secret": "78bca717da",
-                            "server": "8179",
-                            "farm": 9,
-                            "title": "House of Blues Anaheim",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Teragram Ballroom
-                        {
-                            "id": "30043253042",
-                            "owner": "8263900@N06",
-                            "secret": "402dc7f3bd",
-                            "server": "5518",
-                            "farm": 6,
-                            "title": "Okkervil River @ Teragram Ballroom, LA 10-04-2016 28",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
+        $.ajax({
+            url: 'https://api.flickr.com/services/rest',
+            method: 'GET',
+            data: {
+                method: 'flickr.photos.search',
+                api_key: '555bbd6b607a0ffb10da03124b8906b6',
+                format: 'json',
+                nojsoncallback: 1,
+                text: venueLocation
+            },
+            success: function(response) {
+                console.log("Flickr response: ", response);
+                var venueImages = response;
+                var locationId = refList[venueLocation];
 
-                        // The Observatory
-                        {
-                            "id": "22927627330",
-                            "owner": "12694516@N04",
-                            "secret": "b0f524cb25",
-                            "server": "5739",
-                            "farm": 6,
-                            "title": "Broncho plays their final show of their US tour at the Observatory Orange County on Saturaday Nov 21st, 2015. Skinny Puppy, The Shelters, Pearl Charles and others also played the venue this evening.",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Mattress Firm Amphitheatre
-                        {
-                            "id": "37522480132",
-                            "owner": "157274368@N08",
-                            "secret": "57b78471b3",
-                            "server": "4456",
-                            "farm": 5,
-                            "title": "DEPECHE MODE",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Hollywood Pantages Theater
-                        {
-                            "id": "31084523630",
-                            "owner": "42029827@N00",
-                            "secret": "970b9a611c",
-                            "server": "5601",
-                            "farm": 6,
-                            "title": "Hollywood Pantages Theatre",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-
-                        },
-                        // Fivepoint Amphitheatre
-                        {
-                            "id": "36211111572",
-                            "owner": "21024152@N05",
-                            "secret": "52ea661488",
-                            "server": "4352",
-                            "farm": 5,
-                            "title": "KISS 2000 at Irvine Meadows Amphitheater",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        // Shoreline Amphitheatre
-                        {
-                            "id": "39379605704",
-                            "owner": "133744508@N06",
-                            "secret": "a49c8b8f13",
-                            "server": "4602",
-                            "farm": 5,
-                            "title": "Shoreline Amphitheatre.",
-                            "ispublic": 1,
-                            "isfriend": 0,
-                            "isfamily": 0
-                        },
-                        {
-                            "id": "39379605704",
-                        },
-                        // Toyota Amphitheatre
-                        {
-                            "id": "27191296995",
-                        }
-                    ]
-                }
-
-            };
-
-        var refList = {
-            "Mattress Firm Amphitheatre": "37522480132",
-            "Mattress Firm Amphitheatre (Formerly Sleep Train Amphitheatre)": "37522480132",
-            "The Observatory - Santa Ana": "22927627330",
-            "Teragram Ballroom": "30043253042",
-            "House of Blues Anaheim": "8040060204",
-            "Mezzanine": "4484999982",
-            "Fox Theater - Oakland": "31142080295",
-            "Greek Theatre-U.C. Berkeley": "35489133763",
-            "Greek Theatre": "35489133763",
-            "City National Civic": "22592033217",
-            "Bill Graham Civic Auditorium": "30479561483",
-            "Hollywood Palladium": "27020496362",
-            "Hollywood Pantages Theatre": "31084523630",
-            "FivePoint Amphitheatre": "36211111572",
-            "Shoreline Amphitheatre": "39379605704",
-            "Toyota Amphitheatre": "27191296995",
-            "Copper Blues Oxnard Resturant": "",
-            "Dodger Stadium": "",
-            "Save Mart Center": "",
-            "Honda Center": "",
-            "The Forum": "",
-            "The Pacific Amphitheatre": "",
-            "Pechanga Resort and Casino": "",
-            "William Saroyan Theatre Fresno Convention & Entertainment Center": ""
-        };
-
-        var locationId = refList[venueLocation];
-
-        for(var i = 0; i < venueImages.photos.photo.length; i++){
-            if(venueImages.photos.photo[i].id === locationId){
-                displayImage(venueImages.photos.photo[i]);
+                for(var i = 0; i < venueImages.photos.photo.length; i++){
+                    if(venueImages.photos.photo[i].id === locationId){
+                        displayImage(venueImages.photos.photo[i]);
+                    }
+                };
             }
-        };
+        })
     }
 
     displayImage(venueImage){
